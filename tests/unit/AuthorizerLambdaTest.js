5f2514dd7a0f98709415a3024002b605ab5d972f
WooDzu <WooDzu@users.noreply.github.com>
2016-10-09 19:54:03 +0200

    Fix AuthorizerLambda test

--- tests/unit/AuthorizerLambdaTest.js
+++ tests/unit/AuthorizerLambdaTest.js
@@ -1,17 +1,45 @@
 'use strict';
 
+const sinon = require('sinon');
 const expect = require('chai').expect;
+const should = require('chai').should;
 const AuthorizerLambda = require('../../src/AuthorizerLambda');
 
 describe('AuthorizerLambda', () => {
-
   describe('#constructor()', () => {
     it('should attach parameters', () => {
-      const serverless = {sls: 1};
-      const authorizerObj = {auth: 1};
-      const cuthorizerLambda = new AuthorizerLambda(serverless, authorizerObj);
-      expect(typeof cuthorizerLambda.serverless).to.be.equal(serverless);
-      expect(typeof cuthorizerLambda.authorizer).to.be.equal(authorizerObj);
+      const serverless = { service: { prop1: 'val1' } };
+      const authorizerObj = { prop2: 'val2' };
+      const subject = new AuthorizerLambda(serverless, authorizerObj);
+
+      expect(subject).to.have.deep.property('serverless.service.prop1', 'val1');
+      expect(subject).to.have.deep.property('authorizer.prop2', 'val2');
+    });
+  });
+
+  describe('#authorize()', () => {
+    it('should throw errors on missing function', () => {
+      const serverless = {
+        service: {
+          getFunction: sinon.stub().returns(null)
+        }
+      };
+      const authorizer = {name: 'testAuthorizer'};
+      const subject = new AuthorizerLambda(serverless, authorizer);
+
+      expect(() => subject.authorize()).to.throw(Error, 'Authorizer λ testAuthorizer is undefined');
+    });
+
+    it('should throw errors if function has no handler', () => {
+      const serverless = {
+        service: {
+          getFunction: sinon.stub().returns({})
+        }
+      };
+      const authorizer = {name: 'testAuthorizer'};
+      const subject = new AuthorizerLambda(serverless, authorizer);
+
+      expect(() => subject.authorize()).to.throw(Error, 'Authorizer λ testAuthorizer has no handler');
     });
   });
 });
